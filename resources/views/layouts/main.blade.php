c9473af455d7ba975ba121c38ddb474c4ae6a85b
Kancha <kancha@developerenvironment.com>
2021-02-18 12:49:27 +1100

    before auth

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -1,18 +1,21 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="style.css?v=<?php echo time(); ?>">
-    <title>{{config('app.name', 'rti') }}</title>
+    <link rel="stylesheet" type="text/css" href="{{ URL::to('css/style.css') }}">
+    <title>{{ config('app.name', 'rti') }}</title>
 
 
     <!-- bootstrap cdn -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
-    
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css"
+        integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+
 
 </head>
+
 <body>
 
 
@@ -24,8 +27,9 @@
         @include('shared.feedback')
         @yield('content')
     </div>
-    
+
 
 
 </body>
+
 </html>
